8b39d5dc462a609a98698e694c7baab61bcebc3b
Jubayer <jubayeralmahmud24@gmal.com>
2020-12-10 19:12:54 +0600

    Added a new method called greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(String[]args) {
         System.out.println("VS code is Awesome...");
+        greeting("Jubayer");
+    }
+    public static void greeting(String name) {
+        System.out.println("welcome "+name);
     }
 }
